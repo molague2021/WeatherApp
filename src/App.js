8d42797457416b2ee7a2a17ab9c7e9493cbea075
Martin Olague <molague23@hotmail.com>
2022-07-21 20:01:25 -0700

    Adding contextAPI

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import SearchWeather from './components/SearchWeather/SearchWeather';
 import Weather from './components/Weather/Weather';
-
-import weatherApi from './service/weatherApi';
+import WeatherApi, { WeatherApiProvider } from './service/WeatherApi';
 import weatherData from './data/weatherData';
 
 function App() {
   const API_KEY = process.env.REACT_APP_WEATHER_APIKEY;
 
-  const [loading, setLoading] = useState(true);
-  const [temp, setTemp] = useState();
-  const [loc, setLoc] = useState();
-  const [time, setTime] = useState();
-  const [date, setDate] = useState();
+  // const [loading, setLoading] = useState(true);
+  // const [temp, setTemp] = useState();
+  // const [loc, setLoc] = useState();
+  // const [time, setTime] = useState();
+  // const [date, setDate] = useState();
 
   // const getLonandLat = async () => {
   //   const response = await fetch(
@@ -25,56 +24,56 @@ function App() {
   //   return data;
   // };
 
-  const getWeather = async () => {
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=el+paso,texas79938&appid=${API_KEY}&units=imperial`,
-      { method: 'GET' }
-    );
+  // const getWeather = async () => {
+  //   const response = await fetch(
+  //     `https://api.openweathermap.org/data/2.5/weather?q=el+paso,texas79938&appid=${API_KEY}&units=imperial`,
+  //     { method: 'GET' }
+  //   );
 
-    const data = await response.json();
-    console.log(data);
+  //   const data = await response.json();
+  //   console.log(data);
 
-    setTemp(Math.round(data.main.temp) + '\u00B0');
-    setLoc(data.name);
-    setTime(convertToTime(data.dt));
-    setDate(convertToDate(data.dt));
-    setLoading(false);
-  };
+  //   setTemp(Math.round(data.main.temp) + '\u00B0');
+  //   setLoc(data.name);
+  //   setTime(convertToTime(data.dt));
+  //   setDate(convertToDate(data.dt));
+  //   setLoading(false);
+  // };
 
-  const convertToTime = (time) => {
-    var time = new Date(time * 1000).toLocaleTimeString('en-US');
+  // const convertToTime = (time) => {
+  //   var time = new Date(time * 1000).toLocaleTimeString('en-US');
 
-    return time;
-  };
+  //   return time;
+  // };
 
-  const convertToDate = (date) => {
-    var date = new Date(date * 1000).toLocaleDateString('en-US');
-    return date;
-  };
+  // const convertToDate = (date) => {
+  //   var date = new Date(date * 1000).toLocaleDateString('en-US');
+  //   return date;
+  // };
+
+  const { loading, temp, loc, time, date, getWeather } = useContext(WeatherApi);
 
   useEffect(() => {
     getWeather();
-    //setTemp(Math.round(weatherData[0].main.temp) + '\u00B0');
-    //setLoc(weatherData[0].name);
-    //setTime(convertToTime(weatherData[0].dt));
-    //setDate(convertToDate(weatherData[0].dt));
   }, []);
 
   return (
-    <div className="main-container">
-      <div className="main-title-searchicon-container">
-        <div className="title-search-container">
-          <Weather
-            loading={loading}
-            temp={temp}
-            loc={loc}
-            time={time}
-            date={date}
-          />
-          <SearchWeather />
+    <WeatherApiProvider>
+      <div className="main-container">
+        <div className="main-title-searchicon-container">
+          <div className="title-search-container">
+            <Weather
+              loading={loading}
+              temp={temp}
+              loc={loc}
+              time={time}
+              date={date}
+            />
+            <SearchWeather />
+          </div>
         </div>
       </div>
-    </div>
+    </WeatherApiProvider>
   );
 }
 
